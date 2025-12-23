903213e817dc6e2b2f75ddce75b8a4f2c85369e8
khafidhkayyis <khafidhkayyis@gmail.com>
2025-12-23 13:26:29 +0700

    homepage page

---
+++ app/homepage/page.tsx
@@ -0,0 +1,10 @@
+import HeroBanner from "./sections/HeroBanner";
+
+export default function HomePage() {
+    return (
+        <div className="w-full">
+            <HeroBanner />
+        </div>
+    );
+}
+
