7ee49def268ca4c3cfc294a289e0c9f6edaf8f1c
khafidhkayyis <khafidhkayyis@gmail.com>
2026-01-06 09:30:50 +0700

    section-barierName

--- app/whoweare/page.tsx
+++ app/whoweare/page.tsx
@@ -1,10 +1,12 @@
 import WhoWeAre from "./sections/WhoWeAre";
-// import Barrier from "./sections/barrier";
+import BarrierName from "./sections/barrierName";
+
 
 export default function WhoWeArePage() {
     return (
         <div className="w-full">
             <WhoWeAre />
+            <BarrierName />
         </div>
     );
 }
