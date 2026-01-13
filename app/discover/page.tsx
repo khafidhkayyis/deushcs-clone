0482764e86cb113aee38efb1c2c2d27ca004a3c2
khafidhkayyis <khafidhkayyis@gmail.com>
2026-01-13 11:42:45 +0700

    section- video

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -5,11 +5,13 @@ import Fitures from "./sections/fitures";
 import Benefits from "./sections/benefits";
 import KickStart from "./sections/klickStart";
 import Gamified from "./sections/gamified";
+import Video from "./sections/video";
 
 export default function DiscoverPage() {
     return (
         <div className="w-full">
             <BarrierName index={0} />
+            <Video />
             <BarrierName index={1} />
             {/* <BannerVideo /> */}
             <Fitures />
